59a91ba69cd00644accc6aac8d0d97813f29b8ac
Raul-Madalin <raul05madalin@yahoo.com>
2023-06-14 16:47:09 +0300

    Added remember-me and follow-me

--- Refactored v2/Master-Arduino/myPosition.h
+++ Refactored v2/Master-Arduino/myPosition.h
@@ -1,7 +1,8 @@
 struct Position {
   int X, Y, Z;
-  float R;
-} currentPosition, targetPosition;
+  float R, radians;
+  int radius;
+} currentPosition, targetPosition, targetComponent;
 
 void resetPosition(Position& currentPosition) {
   currentPosition.X = 0;
@@ -12,13 +13,13 @@ void resetPosition(Position& currentPosition) {
 
 void printCurrentPosition(int& prevX, int& prevY, int& prevZ, float& prevR) {
   if (prevX != currentPosition.X || prevY != currentPosition.Y || prevZ != currentPosition.Z || prevR != currentPosition.R) {
-    Serial.print("X: ");
-    Serial.print(currentPosition.X);
-    Serial.print(" | ");
+    // Serial.print("X: ");
+    // Serial.print(currentPosition.X);
+    // Serial.print(" | ");
 
-    Serial.print("Y: ");
-    Serial.print(currentPosition.Y);
-    Serial.print(" | ");
+    // Serial.print("Y: ");
+    // Serial.print(currentPosition.Y);
+    // Serial.print(" | ");
 
     Serial.print("Z: ");
     Serial.print(currentPosition.Z);
